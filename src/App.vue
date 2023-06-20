beb3f2259a6d401819acb1b559ea8369ed0b44a8
antoniogisondi <anto.gisondi@tiscali.it>
2023-06-20 14:09:03 +0200

    Start with first components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script>
+import AppFooter from './components/AppFooter.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
 export default {
-  
+  AppHeader,
+  AppMain,
+  AppFooter
 }
 </script>
 
-<template lang="">
+<template>
   <div>
-    
+    <AppHeader/>
+    <AppMain/>
+    <AppFooter/>
   </div>
 </template>
 
-<style lang="">
+<style>
   
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template lang="">
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col"></div>
+                <div class="col"></div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="">
+    
+</style>
