456106477f3c2bda2ec1d114d98dd54eb2f3c611
antoniogisondi <anto.gisondi@tiscali.it>
2023-06-24 11:10:16 +0200

    Exercise done, fixed graphics footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -11,85 +11,82 @@ export default {
 }
 </script>
 
+
 <template lang="">
     <footer>
-        <div class="container-fluid bg-image">
-            <div class="row bg-image-2">
-                <div class="container-col d-flex justify-content-center m-4">
-                    <div class="col-2">
+        <div class="container-top">
+            <div class="row d-flex">
+                <div class="col">
+                    <div class="first-col-list">
                         <ul>
-                            <li>
-                                <h3>DC COMICS</h3>
-                            </li>
-                            <li v-for="item in dc_comics_array">{{ item }}</li>
+                            <h2>DC COMICS</h2>
+                            <li v-for="(item,index) in dc_comics_array" :key="index">{{ item }}</li>
                         </ul>
-    
-                        <ul>
-                            <li>
-                                <h3>SHOP</h3>
-                            </li>
-                            <li v-for="item in shop_array">{{ item }}</li>
-                        </ul>
-                    </div>
-    
-                    <div class="col-2">
-                        <ul>
-                            <li>
-                                <h3>DC</h3>
-                            </li>
-                            <li v-for="item in dc_array">{{ item }}</li>
-                        </ul>
-                    </div>
-    
-                    <div class="col-2">
-                        <ul>
-                            <li>
-                                <h3>SITES</h3>
-                            </li>
-                            <li v-for="item in sites_array">{{ item }}</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="row row-col-3 d-flex flex-row bg-color">
-                <div class="col m-5 d-flex justify-content-start">
-                 <button>SIGN-UP NOW!</button>
-                </div>
 
-                <div class="col d-flex justify-content-end align-items-center">
-                    <h3>FOLLOW US</h3>
-                    <div class="link">
-                        <a href="#">
-                            <img src="../assets/img/footer-facebook.png" alt=""> 
-                        </a>
-                        <a href="#">
-                            <img src="../assets/img/footer-periscope.png" alt="">
-                        </a>
-                        <a href="#">
-                            <img src="../assets/img/footer-pinterest.png" alt="">
-                        </a>
-                        <a href="#">
-                            <img src="../assets/img/footer-twitter.png" alt="">
-                        </a>
-                        <a href="#">
-                            <img src="../assets/img/footer-youtube.png" alt="">
-                        </a>
+                        <ul id="sublist">
+                            <h2>SHOP</h2>
+                            <li v-for="(item,index) in shop_array" :key="index">{{ item }}</li>
+                        </ul>
+                    </div>
+
+                    <div class="second-col-list">
+                        <ul>
+                            <h2>DC </h2>
+                            <li v-for="(item,index) in dc_array" :key="index">{{ item }}</li>
+                        </ul>
+                    </div>
+
+                    <div class="third-col-list">
+                        <ul>
+                            <h2>SITES</h2>
+                            <li v-for="(item,index) in sites_array" :key="index">{{ item }}</li>
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="container-bottom">
+            <div class="row d-flex">
+                <div class="col-button d-flex">
+                    <button>SIGN-UP NOW</button>
+
+                </div>
+                <div class="col-image d-flex justify-content-end">
+                    <h3>FOLLOW US</h3>
+                    <!-- icone social -->
+                    <a href="#">
+                        <img src="../assets/img/footer-facebook.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/footer-periscope.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/footer-pinterest.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/footer-twitter.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/footer-youtube.png" alt="">
+                    </a>
+                </div>
+
+            </div>
+        </div>
+
     </footer>
 </template>
 
-<style lang="scss" scoped>
-.bg-image {
-    background-image: url(../assets/img/footer-bg.jpg);
-    background-repeat: no-repeat;
-    background-size: cover;
 
-    .bg-image-2 {
+<style lang="scss" scoped>
+.container-top {
+    background-image: url('../assets/img/footer-bg.jpg');
+
+
+
+    .row {
         max-width: 1170px;
+        margin: 0 auto;
         background-image: url('../assets/img/dc-logo-bg.png');
         background-size: 500px;
         background-repeat: no-repeat;
@@ -97,49 +94,77 @@ export default {
         padding: 40px 0;
         padding-bottom: 60px;
 
-        h3 {
-            color: white;
-            font-size: 20px;
-            margin: 0;
-        }
-
-        li {
-            list-style-type: none;
-            color: rgba(111, 111, 111, 255);
-            font-size: 15px;
-
-        }
-    }
-}
 
 
+        .col {
+            display: flex;
 
-.bg-color {
-    background-color: rgba(48, 48, 48, 255);
+            ul {
+                list-style-type: none;
+                padding-right: 40px;
+                color: rgba(243, 243, 243, 0.531);
 
-    .col button {
-        background-color: transparent;
-        border-color: rgba(10, 114, 212, 255);
-        padding: 10px;
-        font-size: 15px;
-        font-weight: 700;
-        color: white;
+                li {
+                    &:hover {
+                        color: white;
+                    }
+                }
 
-    }
+                h2 {
+                    padding-bottom: 10px;
+                    color: white;
+                }
+            }
 
-    h3 {
-        color: rgba(2, 130, 249, 255);
-        padding: 20px;
-    }
-
-    .link {
-        a {
-            img {
-                width: 50px;
-                height: 50px;
-                padding: 10px;
+            #sublist {
+                padding-top: 10px;
             }
         }
+    }
+
+
+}
+
+.container-bottom {
+    background-color: rgb(62, 62, 62);
+    padding: 20px 0;
+
+
+    .row {
+        max-width: 1170px;
+        margin: 0 auto;
+
+
+        .col-button {
+            width: 50%;
+
+            button {
+                background-color: transparent;
+                padding: 15px;
+                border-color: rgb(5, 136, 237);
+                color: white;
+                font-weight: 800;
+            }
+
+        }
+
+        .col-image {
+            width: 50%;
+
+            h3 {
+                color: rgb(38, 143, 234);
+                padding-top: 10px;
+                padding-right: 40px;
+            }
+
+            img {
+                margin-right: 10px;
+                height: 30px;
+                margin-top: 5px;
+
+            }
+        }
+
     }
 }
 </style>
